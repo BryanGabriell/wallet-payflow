ce8b24eb32df51d8c6f5224a1f0970ed8f148c04
BryanFinanci <bryanfinanci@gmail.com>
2025-11-06 22:55:11 -0300

    Initial commit

---
+++ src/main/java/com/payflow/wallet/PayFlowDigitalWalletApplication.java
@@ -0,0 +1,13 @@
+package com.payflow.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayFlowDigitalWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayFlowDigitalWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payflow/wallet/PayFlowDigitalWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payflow.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayFlowDigitalWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
